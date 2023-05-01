47f87479567fd9218aeaa92ed0df7d66c06d071a
Edgar Diaz <ed35376@gmail.com>
2023-04-30 21:43:03 -0400

    navbar sticky

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -5,7 +5,7 @@ import React from "react";
 const Navbar= ()=> {
     return(
         <>
-            <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+            <nav className="navbar  navbar-expand-lg  navbar-dark bg-dark sticky-top">
                 <div className="container-fluid ">
                     <a className="navbar-brand text-white" href="#">Start Boostrap</a>
                     <form className="d-flex  ">
