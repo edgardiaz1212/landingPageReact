cf929bb0b2c7678032747a35dc6646e9aafed121
Edgar Diaz <ed35376@gmail.com>
2023-04-30 20:17:30 -0400

    fixed links created Navbar

---
+++ src/js/component/Container.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import Jumbotron from "./Jumbotron.jsx"
+import Card from "./Card.jsx"
+
+export default Container
---
+++ src/js/pages/Home.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Navbar from "../component/Navbar.jsx";
+// import Container from "../component/Container.jsx"
+// import Footer from "../component/Footer.jsx"
+
+
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+const Home = () => {
+	return (
+        <>
+            <Navbar/>
+            <div className="text-center">
+                <h1 className="text-center mt-5">Hello Rigo!</h1>
+                <p>
+                    <img src={rigoImage} />
+                </p>
+                <a href="#" className="btn btn-success">
+                    If you see this green button... bootstrap is working...
+                </a>
+                <p>
+                    Made by{" "}
+                    <a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
+                    love!
+                </p>
+            </div>
+        </>
+	);
+};
+
+
+export default Home
